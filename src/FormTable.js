94db29bfb9d2bd7ddd5d6ba565aeaba350a0a340
v11run <appalasaisuryavarun@gmail.com>
2023-05-21 10:21:49 +0530

    dropdown and search filter added

--- src/FormTable.js
+++ src/FormTable.js
@@ -4,38 +4,70 @@ import jsonData from './data.json';
 
 const FormTable = () => {
   const [employees, setEmployees] = useState(jsonData.employees);
-  const [selectedSkill, setSelectedSkill] = useState('');
+  const [selectedSkills, setSelectedSkills] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [dropdownOpen, setDropdownOpen] = useState(false);
 
   const handleSkillChange = (e) => {
-    setSelectedSkill(e.target.value);
+    const { value, checked } = e.target;
+    if (checked) {
+      setSelectedSkills((prevSkills) => [...prevSkills, value]);
+    } else {
+      setSelectedSkills((prevSkills) => prevSkills.filter((skill) => skill !== value));
+    }
+  };
+
+  const handleSearch = (e) => {
+    setSearchQuery(e.target.value);
   };
 
   const filteredEmployees = employees.filter((employee) => {
-    if (selectedSkill === '') {
-      return true;
-    }
-    return employee.skills.includes(selectedSkill);
+    const hasSelectedSkills =
+      selectedSkills.length === 0 || employee.skills.some((skill) => selectedSkills.includes(skill));
+    const matchesSearchQuery = employee.name && employee.name.toLowerCase().includes(searchQuery.toLowerCase());
+    return hasSelectedSkills && matchesSearchQuery;
   });
+
+  const filteredEmployeesWithNames = filteredEmployees.filter((employee) => employee.name);
+
+  const toggleDropdown = () => {
+    setDropdownOpen((prevState) => !prevState);
+  };
 
   return (
     <div className="form-table-container">
       <div className="filter-container">
-        <label htmlFor="skill-filter">Filter by Skill:</label>
-        <select
+        <label htmlFor="skill-filter">Filter by Skills:</label>
+        <div
           id="skill-filter"
-          value={selectedSkill}
-          onChange={handleSkillChange}
+          className={`dropdown-checkbox ${dropdownOpen ? 'open' : ''}`}
+          onClick={toggleDropdown}
         >
-          <option value="">All</option>
-          <option value="JavaScript">JavaScript</option>
-          <option value="Python">Python</option>
-          <option value="Java">Java</option>
-          <option value="HTML">HTML</option>
-          <option value="CSS">CSS</option>
-          <option value="Photoshop">Photoshop</option>
-          <option value="Manual Testing">Manual Testing</option>
-          <option value="SQL">SQL</option>
-        </select>
+          
+          <label htmlFor="toggle" className="toggle-label">
+            <span className="toggle-text">Select Skills</span>
+            <span className="toggle-arrow">&#9662;</span>
+          </label>
+          <ul className="checkbox-list">
+            {['JavaScript', 'Python', 'Java', 'HTML', 'CSS', 'Photoshop', 'Manual Testing', 'SQL'].map((skill) => (
+              <li key={skill}>
+                <input
+                  type="checkbox"
+                  value={skill}
+                  checked={selectedSkills.includes(skill)}
+                  onChange={handleSkillChange}
+                />
+                <label>{skill}</label>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <input
+          type="text"
+          placeholder="Search by name"
+          value={searchQuery}
+          onChange={handleSearch}
+        />
       </div>
       <table className="form-table">
         <thead>
@@ -44,13 +76,14 @@ const FormTable = () => {
             <th>Name</th>
             <th>Projects</th>
             <th>Designation</th>
+            <th>Skills</th>
           </tr>
         </thead>
         <tbody>
-          {filteredEmployees.map((employee, index) => (
+          {filteredEmployeesWithNames.map((employee, index) => (
             <tr key={index}>
               <td>{employee.id}</td>
-              <td>{employee.name || '-'}</td>
+              <td>{employee.name}</td>
               <td>
                 {employee.projects ? (
                   employee.projects.map((project, projectIndex) => (
@@ -64,12 +97,12 @@ const FormTable = () => {
                 )}
               </td>
               <td>{employee.designation || '-'}</td>
+              <td>{employee.skills.join(', ')}</td>
             </tr>
           ))}
         </tbody>
       </table>
     </div>
   );
-};
-
+                };
 export default FormTable;
